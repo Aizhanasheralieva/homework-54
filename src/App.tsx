3c6b61f96892045a17167f6a3278b8307292f919
Sheralieva Aizhana <asheralieva7@gmail.com>
2025-01-07 13:03:50 +0300

    Wrote css style rules for the blocks items. Draw the board field for the blocks. Also, randomly got the item that is opened.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
 import './App.css';
+import { useState } from 'react';
 
+interface IBlock {
+  hasItem: boolean;
+  id: number;
+  class: 'objectBlock';
+
+}
 const App = () => {
+const [blockItems, setBlockItems] = useState<IBlock[]>([]);
 
+const generateBoardField = () => {
+  const blockObjectArray: IBlock[] = [];
+
+  for (let i = 1; i <= 36; i++) {
+    blockObjectArray.push({
+      hasItem: false,
+      id: i,
+      class: 'objectBlock',
+    });
+  }
+
+  const someRandomIndex = Math.floor(Math.random() * blockObjectArray.length);
+  blockObjectArray[someRandomIndex].hasItem = true;
+
+  setBlockItems(blockObjectArray);
+};
+
+if (blockItems.length === 0) {
+  generateBoardField();
+} else {
+  console.log(blockItems);
+}
   return (
     <>
+      <div className="squareField">
+        {blockItems.map(blockItem => (
+          <div className={blockItem.class}></div>
+        ))}
+
+      </div>
+
 
     </>
   );
